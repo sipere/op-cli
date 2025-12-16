7115e4ed431aa6fcbaaa3195010344aab8bc7cbe
Sallai András <andrewteki@gmail.com>
2025-12-16 18:15:05 +0100

    Added pluralizer

--- templates/modelTemplate.js
+++ templates/modelTemplate.js
@@ -3,6 +3,9 @@ import sequelize from '../database/database.js'
 
 const ModelName = sequelize.define('TableBaseName', {
     name: { type: DataTypes.STRING,  allowNull: false  }
+}, {
+    timestamps: true,
+    freezeTableName: true
 })
 
 export default ModelName
